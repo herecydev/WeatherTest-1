99412b7444020eb4b98478ff8da538d7087aa2f7
Matthew Ridgway <matt@mattridgway.onmicrosoft.com>
2016-02-02 23:15:44 +0000

    Added MVC and random weather result returned

---
+++ src/WeatherTest.Bbc/Controllers/WeatherController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.Mvc;
+using WeatherTest.Bbc.Models;
+
+namespace WeatherTest.Bbc.Controllers
+{
+    public class WeatherController : Controller
+    {
+		private System.Random _rng = new System.Random();
+
+		[HttpGet]
+		[Route("[controller]/{location}")]
+		public WeatherResult Get(string location)
+			=> new WeatherResult
+			{
+				Location = location,
+				TemperatureFahrenheit = _rng.Next(32, 100),
+				WindSpeedMph = _rng.Next(0, 20)
+			};
+	}
+}
