7bf5307177be8ace1e8cad14e673554d69ffa7c6
Matthew Ridgway <matt@mattridgway.onmicrosoft.com>
2016-02-02 23:33:30 +0000

    Added project for provider tests

---
+++ test/WeatherTest.Accu.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WeatherTest.Accu.Tests")]
+[assembly: AssemblyDescription("Test library for WeatherTest AccuWeather provider")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Matt Ridgway")]
+[assembly: AssemblyProduct("WeatherTest")]
+[assembly: AssemblyCopyright("Copyright © Matt Ridgway 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("2ea96076-ab43-4c6d-9895-27012ca2a727")]
