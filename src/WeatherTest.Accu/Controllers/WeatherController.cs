e0dec6d3c04e701a6b5fc6c54caebaf5c920c3b6
Matthew Ridgway <matt@mattridgway.onmicrosoft.com>
2016-02-02 22:58:35 +0000

    Added MVC, controller and rng to give random values for weather

---
+++ src/WeatherTest.Accu/Controllers/WeatherController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Mvc;
+using WeatherTest.Accu.Models;
+
+namespace WeatherTest.Accu.Controllers
+{
+	public class WeatherController : Controller
+    {
+		private System.Random _rng = new System.Random();
+
+		[HttpGet]
+		[Route("[controller]/{location}")]
+		public WeatherResult Get(string location)
+			=> new WeatherResult {
+				Location = location,
+				TemperatureCelsius = _rng.Next(0, 38),
+				WindSpeedKph = _rng.Next(0, 32) };
+    }
+}
--- src/WeatherTest.Accu/Startup.cs
+++ src/WeatherTest.Accu/Startup.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace WeatherTest.Accu
@@ -9,16 +8,13 @@ namespace WeatherTest.Accu
     {
         public void ConfigureServices(IServiceCollection services)
         {
+			services.AddMvc();
         }
 		
         public void Configure(IApplicationBuilder app)
         {
             app.UseIISPlatformHandler();
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+			app.UseMvc();
         }
 		
         public static void Main(string[] args) => WebApplication.Run<Startup>(args);
