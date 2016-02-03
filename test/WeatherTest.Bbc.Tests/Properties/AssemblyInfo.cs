c398130d21b97bfe4ada04ca6d7641abaa81bc0a
Matthew Ridgway <matt@mattridgway.onmicrosoft.com>
2016-02-03 00:13:59 +0000

    Added test project for fake BBC weather provider

---
+++ test/WeatherTest.Bbc.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WeatherTest.Bbc.Tests")]
+[assembly: AssemblyDescription("Test library for WeatherTest BbcWeather provider")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Matt Ridgway")]
+[assembly: AssemblyProduct("WeatherTest")]
+[assembly: AssemblyCopyright("Copyright © Matt Ridgway 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("5833f7ba-98ab-40df-b773-c3c1d8ee42da")]
